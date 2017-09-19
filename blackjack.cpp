900a0335c5d295c474a1d54c20d5f707138096fc
Crouse <peter.crouse@debeersgroup.com>
2017-09-19 14:20:49 +0200

    updated some QActions at work

--- blackjack.cpp
+++ blackjack.cpp
@@ -20,14 +20,15 @@ BlackJack::BlackJack(QWidget *parent) :
     actionGroup->addAction(ui->action_Hit_ME);
     actionGroup->addAction(ui->action_New_Game);
     actionGroup->addAction(ui->action_Quit);
-    actionGroup->addAction(ui->action_Shuffle_Deck);
     actionGroup->addAction(ui->action_Stay);
+
+    ui->action_Hit_ME->setEnabled(false);
+    ui->action_Stay->setEnabled(false);
+    ui->action_Deal_Hand->setEnabled(false);
 
     QToolBar* toolbar = new QToolBar("Choice ToolBar", this);
     toolbar->addActions(actionGroup->actions());
     this->addToolBar(Qt::RightToolBarArea, toolbar);
-
-    deal();
 
     ui->playerLayout->addWidget(playerView);
     ui->dealerLayout->addWidget(dealerView);
@@ -56,9 +57,20 @@ void BlackJack::actionEvent(QAction *event){
     }
     if(name == "action_New_Game"){
         deck.reset();
-        while ( Card* c = player->findChild<Card*>())
-            delete c;
+        while ( Card* p = player->findChild<Card*>())
+            delete p;
+        while ( Card* d = dealer->findChild<Card*>())
+            delete d;
+    }
+    if(name == "action_Deal_Hand"){
         deal();
+    }
+    if(name == "Stay"){
+        int score = 0;
+        QList<Card*> cards = player->findChildren<Card*>();
+        for(int i = 0; i < cards.length(); i++){
+            score += cards.at(i)->value();
+        }
     }
 
 }
